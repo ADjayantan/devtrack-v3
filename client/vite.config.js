bf82fa88072094e15554e27e345673af9f347985
Jayantan AD <adjayantan2007@gmail.com>
2026-05-07 12:50:21 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,9 +3,11 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: process.env.VITE_BASE || '/',
   build: {
     outDir: 'dist',
     emptyOutDir: true,
+    chunkSizeWarningLimit: 800, // suppress the 500kb warning
   },
   server: {
     proxy: {
