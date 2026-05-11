f2f49b133fbce31f71a617e8009cf56a03aa1cf4
Jayantan AD <adjayantan2007@gmail.com>
2026-05-11 11:38:19 +0530

    Update logs.js

--- server/routes/logs.js
+++ server/routes/logs.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const { getLogs, createLog, updateLog, deleteLog, getAnalytics, exportCSV } = require('../controllers/logController');
 const { protect } = require('../middleware/auth');
 const { apiLimiter } = require('../middleware/rateLimiter');
-const { logRules, logQueryRules, validate } = require('../utils/validators');
+const { logRules, logUpdateRules, logQueryRules, validate } = require('../utils/validators');
 
 router.use(protect);
 router.use(apiLimiter);
@@ -12,7 +12,7 @@ router.get('/analytics', getAnalytics);   // must be before /:id routes
 router.get('/export',    exportCSV);
 router.get('/',          logQueryRules, validate, getLogs);
 router.post('/',         logRules, validate, createLog);
-router.put('/:id',       logRules, validate, updateLog);
+router.put('/:id',       logUpdateRules, validate, updateLog);  // Fix: was using logRules which forced date on every PUT
 router.delete('/:id',    deleteLog);
 
 module.exports = router;
